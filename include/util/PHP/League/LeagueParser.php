b92069d50979384fea5f03f2a06d1902b452dd35
Trym <chinzcoding@gmail.com>
2024-03-15 14:11:03 +0100

    feat: Add LeagueParser class for parsing league API responses
    - Extracts relevant information such as league ID, name, logo, country and seasons from the API response.

---
+++ include/util/PHP/League/LeagueParser.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Infoball\util\PHP\League;
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/config/setup.php';
+
+class LeagueParser
+{
+    public function parseApiResponse(array $apiResponse): array
+    {
+        // Extract the relevant data from the API response
+        $leagueData = $apiResponse[0]['league'];
+        $countryData = $apiResponse[0]['country'];
+        $leagueSeasons = $apiResponse[0]['seasons'];
+
+        $leagueId = $leagueData['id'];
+        $leagueName = $leagueData['name'];
+        $leagueLogo = $leagueData['logo'];
+        $countryName = $countryData['name'];
+
+        $leagueSeasonsYear = [];
+
+        foreach ($leagueSeasons as $season) {
+            $leagueSeasonsYear[] = $season['year'];
+        }
+
+        // Return the parsed data
+        return [
+            'id' => $leagueId,
+            'name' => $leagueName,
+            'logo' => $leagueLogo,
+            'country' => $countryName,
+            'seasons' => $leagueSeasonsYear,
+        ];
+    }
+}
