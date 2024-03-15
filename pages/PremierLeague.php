b1296d06863b2bc2d0451b5584afb1edc8f50d6a
Trym <chinzcoding@gmail.com>
2024-03-15 14:12:25 +0100

    chore: Update PremierLeague page to test new classes and display data

--- pages/PremierLeague.php
+++ pages/PremierLeague.php
@@ -6,6 +6,9 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/config/setup.php';
 
 use Infoball\classes\Base\Base;
 use Infoball\classes\Api\LeaguesApiClient;
+use Infoball\util\PHP\League\LeagueParser;
+use Infoball\util\PHP\DataHandler\DataHandler;
+use Infoball\classes\Database\DatabaseManager;
 
 /**
  * Class PremierLeague
@@ -24,42 +27,35 @@ class PremierLeague extends Base
         // Render the PremierLeague page.
         if (isset($_SESSION["userId"])) {
             echo $this->render("/classes/PremierLeague/PremierLeague.html.twig", [
-                "data" => $this->getData(),
+                "data" => $this->getLeagueData(),
             ]);
         } else {
             header("Location: /pages/Login.php");
         }
     }
 
-    private function getData()
+    private function getLeagueData()
     {
         $apiKey = 'cd49181cf69486c1ac135da582e54070';
         $baseUrl = 'https://v3.football.api-sports.io/leagues';
         $leaguesApiClient = new LeaguesApiClient($apiKey, $baseUrl);
+        $leagueParser = new LeagueParser();
+        $databaseManager = new DatabaseManager();
 
-        $paramName = 'Premier league';
-        $paramCountry = 'England';
-        $data = $leaguesApiClient->fetchLeagueData($paramName, $paramCountry);
+        $dataHandler = new DataHandler($leaguesApiClient, $leagueParser, $databaseManager);
 
-        if ($data !== false) {
-            $leagueData = $data[0]['league'];
-            $name = $leagueData['name'];
-            $logo = $leagueData['logo'];
+        $name = 'Premier League';
+        $country = 'England';
+        // $dataHandler-> handleDataFetchingAndStoring($name, $country);
+        $leagueData = $dataHandler->handleRetrievingDataFromDb($name, $country);
 
-            $country = $data[0]['country']['name'];
-
-            $seasons = $data[0]['seasons'];
-
-            return [
-                'name' => $name,
-                'logo' => $logo,
-                'country' => $country,
-                'seasons' => $seasons,
-            ];
-        } else {
-            //Handle case where data retrieval fails
-            return [];
-        }
+        return [
+            'id' => $leagueData->getId(),
+            'name' => $leagueData->getName(),
+            'logo' => $leagueData->getLogo(),
+            'country' => $leagueData->getCountry(),
+            'seasons' => $leagueData->getSeasons(),
+        ];
     }
 }
 
