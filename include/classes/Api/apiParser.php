d03081fafee69db0497235deecc80c6b55362ba5
Trym <chinzcoding@gmail.com>
2024-03-20 15:13:11 +0100

    feat: Add parseStandingsApiResponse to apiParser Class
    - Added `parseStandingsApiResponse` method to parse the standings API response and extract relevant data for standings.

--- include/classes/Api/apiParser.php
+++ include/classes/Api/apiParser.php
@@ -6,6 +6,35 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/config/setup.php';
 
 class apiParser
 {
+    public function parseStandingsApiResponse(array $apiResponse): array
+    {
+        $parsedStandings = [];
+
+        $leagueData = $apiResponse[0]['league'];
+
+        foreach ($leagueData['standings'] as $standings) {
+            foreach ($standings as $standing) {
+                $parsedStandings[] = [
+                    'rank' => $standing['rank'],
+                    'teamId' => $standing['team']['id'],
+                    'name' => $standing['team']['name'],
+                    'logo' => $standing['team']['logo'],
+                    'points' => $standing['points'],
+                    'goalsDiff' => $standing['goalsDiff'],
+                    'form' => $standing['form'],
+                    'played' => $standing['all']['played'],
+                    'win' => $standing['all']['win'],
+                    'draw' => $standing['all']['draw'],
+                    'lose' => $standing['all']['lose'],
+                    'goalsFor' => $standing['all']['goals']['for'],
+                    'goalsAgainst' => $standing['all']['goals']['against']
+                ];
+            }
+        }
+
+        return $parsedStandings;
+    }
+
     public function parseLeagueApiResponse(array $apiResponse): array
     {
         // Extract the relevant data from the API response
