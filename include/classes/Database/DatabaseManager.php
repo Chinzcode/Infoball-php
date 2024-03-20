1716db63f017ab14ae65189da3cc286047fbe579
Trym <chinzcoding@gmail.com>
2024-03-20 15:11:59 +0100

    feat: Add insertStanding and getStanding methods
    - Added `insertStanding` method to insert standings data into the database.
    - Added `getStanding` method to retrieve standings data from the database.

--- include/classes/Database/DatabaseManager.php
+++ include/classes/Database/DatabaseManager.php
@@ -5,6 +5,7 @@ namespace Infoball\classes\Database;
 use PDO;
 use Infoball\util\PHP\League\League;
 use Infoball\classes\Database\Database;
+use Infoball\util\PHP\Standing\Standing;
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/config/setup.php';
 
@@ -14,6 +15,65 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/config/setup.php';
  */
 class DatabaseManager
 {
+    public function insertStanding(Standing $standing, int $league, int $season)
+    {
+        $db = Database::getDb();
+
+        $tableName = "leagueStandings" . $league . "_" . $season;
+
+        $query = "INSERT INTO $tableName (team_rank, team_id, name, logo, points, goals_diff, form, played, win, draw, lose, goals_for, goals_against) 
+                  VALUES (:team_rank, :team_id, :name, :logo, :points, :goals_diff, :form, :played, :win, :draw, :lose, :goals_for, :goals_against)";
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(":team_rank", $standing->getRank());
+        $stmt->bindValue(":team_id", $standing->getId());
+        $stmt->bindValue(":name", $standing->getName());
+        $stmt->bindValue(":logo", $standing->getLogo());
+        $stmt->bindValue(":points", $standing->getPoints());
+        $stmt->bindValue(":goals_diff", $standing->getGoalsDiff());
+        $stmt->bindValue(":form", $standing->getForm());
+        $stmt->bindValue(":played", $standing->getPlayed());
+        $stmt->bindValue(":win", $standing->getWin());
+        $stmt->bindValue(":draw", $standing->getDraw());
+        $stmt->bindValue(":lose", $standing->getLose());
+        $stmt->bindValue(":goals_for", $standing->getGoalsFor());
+        $stmt->bindValue(":goals_against", $standing->getGoalsAgainst());
+        $stmt->execute();
+    }
+
+    public function getStanding(int $league, int $season)
+    {
+        $db = Database::getDb();
+
+        $tableName = "leagueStandings" . $league . "_" . $season;
+
+        $query = "SELECT * FROM $tableName";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+
+        $standings = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $standing = new Standing(
+                $row['team_rank'],
+                $row['team_id'],
+                $row['name'],
+                $row['logo'],
+                $row['points'],
+                $row['goals_diff'],
+                $row['form'],
+                $row['played'],
+                $row['win'],
+                $row['draw'],
+                $row['lose'],
+                $row['goals_for'],
+                $row['goals_against']
+            );
+
+            $standings[] = $standing;
+        }
+
+        return $standings;
+    }
+
     public function insertLeague(League $league)
     {
         $db = Database::getDb();
