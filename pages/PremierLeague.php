f069ab29623ebdfd6d679c44867e5553f20e6529
Trym <chinzcoding@gmail.com>
2024-03-20 15:14:33 +0100

    feat: Add getStandingData method
    - Implemented the `getStandingData` method to fetch standing data from the API or the database and return it for rendering.

--- pages/PremierLeague.php
+++ pages/PremierLeague.php
@@ -4,11 +4,13 @@ namespace Pages;
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/config/setup.php';
 
+use Dotenv\Dotenv;
 use Infoball\classes\Base\Base;
-use Infoball\classes\Api\LeaguesApiClient;
 use Infoball\classes\Api\apiParser;
-use Infoball\util\PHP\DataHandler\DataHandler;
+use Infoball\classes\Api\LeaguesApiClient;
+use Infoball\classes\Api\StandingsApiClient;
 use Infoball\classes\Database\DatabaseManager;
+use Infoball\util\PHP\DataHandler\DataHandler;
 
 /**
  * Class PremierLeague
@@ -28,23 +30,53 @@ class PremierLeague extends Base
         if (isset($_SESSION["userId"])) {
             echo $this->render("/classes/PremierLeague/PremierLeague.html.twig", [
                 "data" => $this->getLeagueData(),
+                "standingsData" => $this->getStandingData(),
             ]);
         } else {
             header("Location: /pages/Login.php");
         }
     }
 
+    private function getStandingData()
+    {
+        // Load the environment variables from the .env file
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+        $dotenv->load();
+
+        // Fetch API key and base URL from environment variables
+        $apiKey = $_ENV['API_KEY'];
+        $baseUrl = 'https://v3.football.api-sports.io/standings';
+
+        $standingsApiClient = new StandingsApiClient($apiKey, $baseUrl);
+        $apiParser = new apiParser();
+        $databaseManager = new DatabaseManager();
+
+        $dataHandler = new DataHandler($apiParser, $databaseManager);
+
+        $league = 39;
+        $season = 2023;
+
+        // $dataHandler->handleStandingsDataFetchingAndStoring($standingsApiClient, $league, $season);
+        $standingsData = $dataHandler->handleRetrievingStandingsDataFromDb($league, $season);
+
+        return $standingsData;
+    }
+
     private function getLeagueData()
     {
+        // Load the environment variables from the .env file
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+        $dotenv->load();
+
         // Fetch API key and base URL from environment variables
-        $apiKey = getenv('API_KEY');
-        $baseUrl = getenv('BASE_URL');
+        $apiKey = $_ENV['API_KEY'];
+        $baseUrl = 'https://v3.football.api-sports.io/leagues';
 
         $leaguesApiClient = new LeaguesApiClient($apiKey, $baseUrl);
         $apiParser = new apiParser();
         $databaseManager = new DatabaseManager();
 
-        $dataHandler = new DataHandler($leaguesApiClient, $apiParser, $databaseManager);
+        $dataHandler = new DataHandler($apiParser, $databaseManager);
 
         $name = 'Premier League';
         $country = 'England';
