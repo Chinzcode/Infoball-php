2cd83937e5f44789563f2167c46c1d8997793eb4
Trym <chinzcoding@gmail.com>
2024-04-10 14:02:06 +0200

    feat(data-handler): Add TopscorerDataHandler class
    Added TopscorerDataHandler class responsible for retrieving topscorer data from database.

---
+++ include/util/PHP/DataHandler/TopscorerDataHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Infoball\util\PHP\DataHandler;
+
+use Infoball\classes\Api\ApiParser;
+use Infoball\classes\Database\DatabaseManager;
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/config/setup.php';
+
+class TopscorerDataHandler
+{
+    protected ApiParser $parser;
+    protected DatabaseManager $databaseManager;
+    protected string $apiKey;
+    protected string $url;
+
+    public function __construct(ApiParser $parser, DatabaseManager $databaseManager, string $apiKey, string $url)
+    {
+        $this->parser = $parser;
+        $this->databaseManager = $databaseManager;
+        $this->apiKey = $apiKey;
+        $this->url = $url;
+    }
+
+    public function getTopscorerData(int $league, int $season, string $name): array
+    {
+        $dataHandler = new DataHandler($this->parser, $this->databaseManager);
+        $goalData = $dataHandler->handleRetrievingTopscorerDataFromDb($league, $season, $name);
+
+        return $goalData;
+    }
+}
