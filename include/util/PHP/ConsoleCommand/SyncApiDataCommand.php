8bdd92906e93c07c48f4e736fd12be72e30e7b60
Trym <chinzcoding@gmail.com>
2024-04-22 10:19:33 +0200

    feat: Update SyncApiDataCommand class

--- include/util/PHP/ConsoleCommand/SyncApiDataCommand.php
+++ include/util/PHP/ConsoleCommand/SyncApiDataCommand.php
@@ -2,11 +2,11 @@
 
 namespace Infoball\util\PHP\ConsoleCommand;
 
+use Infoball\classes\DataUpdater\DataUpdater;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Infoball\classes\DataUpdater\StandingsDataUpdater;
 
 #[AsCommand(
     name: 'syncApiData',
@@ -18,9 +18,40 @@ class SyncApiDataCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $url = [
+            $standings = 'https://v3.football.api-sports.io/standings',
+            $topscorers = 'https://v3.football.api-sports.io/players/topscorers',
+            $topassists = 'https://v3.football.api-sports.io/players/topassists',
+            $topyellowcards = 'https://v3.football.api-sports.io/players/topyellowcards',
+            $topredcards = 'https://v3.football.api-sports.io/players/topredcards',
+            $fixtures = 'https://v3.football.api-sports.io/fixtures',
+        ];
 
-        $standingsDataUpdater = new StandingsDataUpdater();
-        $standingsDataUpdater->updateStandingsData();
+        $season = [
+            $year = 2023,
+            $startDate = '2023-08-11',
+            $endDate = '2024-05-19',
+        ];
+
+        $league = 39;
+
+        $updateData = new DataUpdater($url.$standings);
+        $updateData->updateStandingsData($league, $season.$year);
+
+        $updateData = new DataUpdater($url.$topscorers);
+        $updateData->updatePlayerstatsData($league, $season.$year, 'topscorer');
+
+        $updateData = new DataUpdater($url.$topassists);
+        $updateData->updatePlayerstatsData($league, $season.$year, 'topassists');
+
+        $updateData = new DataUpdater($url.$topyellowcards);
+        $updateData->updatePlayerstatsData($league, $season.$year, 'topyellowcards');
+
+        $updateData = new DataUpdater($url.$topredcards);
+        $updateData->updatePlayerstatsData($league, $season.$year, 'topredcards');
+
+        $updateData = new DataUpdater($url.$fixtures);
+        $updateData->updateFixturesData($league, $season.$year, 'allFixtures', $season.$startDate, $season.$endDate);
 
         $output->writeln([
             '',
