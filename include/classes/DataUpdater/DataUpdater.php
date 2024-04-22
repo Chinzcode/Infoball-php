74155851d6c4fdce2e983e6f60648a95d1a4bffb
Trym <chinzcoding@gmail.com>
2024-04-22 10:19:59 +0200

    feat(data-updater): Update DataUpdater class

--- include/classes/DataUpdater/DataUpdater.php
+++ include/classes/DataUpdater/DataUpdater.php
@@ -30,12 +30,6 @@ class DataUpdater
         $this->dataHandler = new DataHandler($this->apiParser, $this->databaseManager);
     }
 
-    public function updateFixturesData(int $league, int $season, string $name, string $startDate, string $endDate)
-    {
-        $this->fixturesApiClient = new FixturesApiClient($this->apiKey, $this->url);
-        $this->dataHandler->handleFixturesDataFetchingAndStoring($this->fixturesApiClient, $league, $season, $name, $startDate, $endDate);
-    }
-
     public function updateStandingsData(int $league, int $season) //league = 39, season = 2023
     {
         $this->standardParamApiClient = new StandardParamApiClient($this->apiKey, $this->url);
@@ -47,22 +41,10 @@ class DataUpdater
         $this->standardParamApiClient = new StandardParamApiClient($this->apiKey, $this->url);
         $this->dataHandler->handlePlayerstatsDataFetchingAndStoring($this->standardParamApiClient, $league, $season, $name);
     }
+
+    public function updateFixturesData(int $league, int $season, string $name, string $startDate, string $endDate)
+    {
+        $this->fixturesApiClient = new FixturesApiClient($this->apiKey, $this->url);
+        $this->dataHandler->handleFixturesDataFetchingAndStoring($this->fixturesApiClient, $league, $season, $name, $startDate, $endDate);
+    }
 }
-
-// $updateData = new DataUpdater('https://v3.football.api-sports.io/standings');
-// $updateData->updateStandingsData(39, 2023);
-
-// $updateData = new DataUpdater('https://v3.football.api-sports.io/players/topscorers');
-// $updateData->updatePlayerstatsData(39, 2023, 'topscorer');
-
-// $updateData = new DataUpdater('https://v3.football.api-sports.io/players/topassists');
-// $updateData->updatePlayerstatsData(39, 2023, 'topassists');
-
-// $updateData = new DataUpdater('https://v3.football.api-sports.io/players/topyellowcards');
-// $updateData->updatePlayerstatsData(39, 2023, 'topyellowcards');
-
-// $updateData = new DataUpdater('https://v3.football.api-sports.io/players/topredcards');
-// $updateData->updatePlayerstatsData(39, 2023, 'topredcards');
-
-// $updateData = new DataUpdater('https://v3.football.api-sports.io/fixtures');
-// $updateData->updateFixturesData(39, 2023, 'allFixtures', '2023-08-11', '2024-05-19');
