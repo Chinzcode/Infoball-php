e62dfd76724463cc0df01e9437828f992acb12dd
tryms <tryms@coretrek.no>
2024-11-11 15:44:45 +0100

    fix(Infoball): Update require_once expression to include correct namespace

--- include/classes/Database/Database.php
+++ include/classes/Database/Database.php
@@ -2,7 +2,7 @@
 
 namespace Infoball\classes\Database;
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/config/setup.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Infoball/config/setup.php';
 
 use PDO;
 use PDOException;
